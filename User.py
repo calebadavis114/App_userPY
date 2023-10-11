2939f9f34e318b2adf56c18a0a34670bd9ccc201
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-11 15:33:03 -0700

    remastered 11

--- User.py
+++ User.py
@@ -1,35 +1,35 @@
 class Users:
+    posts = []
     user_manager = {}
     def __init__(self, name, email, dlnumber):
         self.name = name  
         self.email = email
         self._dlnumber = dlnumber
+        self.post = "this is my first post"
+
         Users.user_manager[self.name] = self
    
     def __repr__(self):
-        return (f"Name: {self.name} | Email: {self.email} | DL Number: {self._dlnumber}")
+        return (f"Name: {self.name} | Email: {self.email} | posts : {Users.posts}")
     @property
     def get_dlnumber(self):
        return self._dlnumber
-    
-    @get_dlnumber.setter
-    def set_dlnumber(self, dlnumber):
-        if(type(dlnumber) == int):
-            self._dlnumber = dlnumber
    
     @classmethod
-    def add_user(cls):
+    def add_post(cls):
         name = input("Name: ")
         email = input("Email Address: ")
-        dlnumber = input("DLNumber: ")
-        cls(name, email, dlnumber)
+        post = input("What would you like to post? ")
+        Users.posts.append(post)
+        cls(name, email, post)
     
     @staticmethod
     def view_all_users():
         for user in Users.user_manager.values():
             print(repr(user))
     def intro(self):
-        print(f"Hi, my name is {self.name}, my email address is {self.email} and my driver's licence number is {self._dlnumber}")
+        print(f"Hi, my name is {self.name}, my email address is {self.email} : Here's what I'm up to right now: {self.post}")
+    
 
 caleb = Users("Caleb", "caleb.a.davis114@gmail.com", 292928343)
 julia = Users("Julia", "juliadias@gmail.com", 4985673)
@@ -39,6 +39,5 @@ caleb.intro()
 steven.intro()
 
 while True:
-    Users.add_user()
+    Users.add_post()
     Users.view_all_users()
-
