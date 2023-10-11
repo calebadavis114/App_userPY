60787266d5336cb71515290c2cecc3aebf93d0d8
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-11 13:51:31 -0700

    started

--- User.py
+++ User.py
@@ -1,1 +1,44 @@
-# your improved User class goes here
+class Users:
+    user_manager = {}
+    def __init__(self, name, email, dlnumber):
+        self.name = name  
+        self.email = email
+        self._dlnumber = dlnumber
+        Users.user_manager[self.name] = self
+   
+    def __repr__(self):
+        return (f"Name: {self.name} | Email: {self.email} | DL Number: {self._dlnumber}")
+    @property
+    def get_dlnumber(self):
+       return self._dlnumber
+    
+    @get_dlnumber.setter
+    def set_dlnumber(self, dlnumber):
+        if(type(dlnumber) == int):
+            self._dlnumber = dlnumber
+   
+    @classmethod
+    def add_user(cls):
+        name = input("Name: ")
+        email = input("Email Address: ")
+        dlnumber = input("DLNumber: ")
+        cls(name, email, dlnumber)
+    
+    @staticmethod
+    def view_all_users():
+        for user in Users.user_manager.values():
+            print(repr(user))
+    def intro(self):
+        print(f"Hi, my name is {self.name}, my email address is {self.email} and my driver's licence number is {self._dlnumber}")
+
+caleb = Users("Caleb", "caleb.a.davis114@gmail.com", 292928343)
+julia = Users("Julia", "juliadias@gmail.com", 4985673)
+steven = Users(email="steventhekingsen@gmail.com",dlnumber=29304058,name="Steven")
+julia.intro()
+caleb.intro()
+steven.intro()
+
+while True:
+    Users.add_user()
+    Users.view_all_users()
+
